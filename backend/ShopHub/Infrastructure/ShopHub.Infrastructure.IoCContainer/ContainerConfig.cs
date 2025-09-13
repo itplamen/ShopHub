d53133c548499e84b3a3f0b1e3db6974ea6647f9
itplamen <itplamen@gmail.com>
2025-09-13 13:21:51 +0300

    Created IoC config and packages

---
+++ backend/ShopHub/Infrastructure/ShopHub.Infrastructure.IoCContainer/ContainerConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace ShopHub.Infrastructure.IoCContainer
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    using ShopHub.Infrastructure.IoCContainer.Contracts;
+    using ShopHub.Infrastructure.IoCContainer.Packages;
+
+    public static class ContainerConfig
+    {
+        public static void AddWebServices(this IServiceCollection services)
+        {
+            IPackage[] packages = new IPackage[]
+            {
+                
+                new MappingsPackage(),
+                new WebPackage(),
+            };
+
+            RegisterServices(services, packages);
+        }
+
+        private static void RegisterServices(IServiceCollection services, IPackage[] packages)
+        {
+            foreach (var package in packages)
+            {
+                package.RegisterServices(services);
+            }
+        }
+    }
+}
---
+++ backend/ShopHub/Infrastructure/ShopHub.Infrastructure.IoCContainer/Contracts/IPackage.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopHub.Infrastructure.IoCContainer.Contracts
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public interface IPackage
+    {
+        void RegisterServices(IServiceCollection services);
+    }
+}
