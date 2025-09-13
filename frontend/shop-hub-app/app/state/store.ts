240f07781929441212d0245b98c12347a3c314dc
itplamen <itplamen@gmail.com>
2025-09-13 23:30:20 +0300

    Registered shopping cart slice

--- frontend/shop-hub-app/app/state/store.ts
+++ frontend/shop-hub-app/app/state/store.ts
@@ -2,16 +2,18 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 
 import authReducer from "./slices/authSlice";
+import shoppingCartReducer from "./slices/shoppingCartSlice";
 import storage from "./storage";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["auth"],
+  whitelist: ["auth", "shoppingCart"],
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  shoppingCart: shoppingCartReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
