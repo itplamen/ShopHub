2f6d49569db4e5cd1d531fed34eaf101f4135b93
itplamen <itplamen@gmail.com>
2025-09-14 02:48:51 +0300

    Improved auth actions

--- frontend/shop-hub-app/app/actions/authActions.ts
+++ frontend/shop-hub-app/app/actions/authActions.ts
@@ -1,30 +1,41 @@
-import { post } from "~/api/shopHubApi";
+import { postData } from "~/api/shopHubApi";
 import type { Auth } from "~/models/data";
+import type {
+  SignInRequest,
+  SignOutRequest,
+  SignUpRequest,
+} from "~/models/request";
+import type { AuthResponse } from "~/models/response";
 
 const deviceId = "1";
 const deviceName = "web";
 
 const signUp = async (
-  url: string,
   username: string,
   password: string,
-  fullName: string
-): Promise<ApiResponse<string>> => {
+  fullName: string,
+  age: number,
+  city: string
+): Promise<AuthResponse<string>> => {
   const request: SignUpRequest = {
     username,
     password,
     fullName,
+    age,
+    city,
   };
-  const response = await post<SignUpRequest, string>(url, request);
+  const response = await postData<SignUpRequest, string>(
+    "http://localhost:5041/api/auth/register",
+    request
+  );
 
-  return response;
+  return { ...response, type: "SignUp" };
 };
 
 const signIn = async (
-  url: string,
   username: string,
   password: string
-): Promise<ApiResponse<Auth>> => {
+): Promise<AuthResponse<Auth>> => {
   const request: SignInRequest = {
     username,
     password,
@@ -32,8 +43,27 @@ const signIn = async (
     deviceName,
   };
 
-  const response = await post<SignInRequest, Auth>(url, request);
-  return response;
+  const response = await postData<SignInRequest, Auth>(
+    "http://localhost:5041/api/auth/login",
+    request
+  );
+
+  return { ...response, type: "SignIn" };
 };
 
-export { signUp, signIn };
+const signOut = async (
+  refreshToken: string
+): Promise<AuthResponse<boolean>> => {
+  const request: SignOutRequest = {
+    refreshToken,
+  };
+
+  const response = await postData<SignOutRequest, boolean>(
+    "http://localhost:5041/api/auth/logout",
+    request
+  );
+
+  return { ...response, type: "SignOut" };
+};
+
+export { signUp, signIn, signOut };
