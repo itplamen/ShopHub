b0d97f508afaa9610ab165bc8dac43aacf0ed1c5
itplamen <itplamen@gmail.com>
2025-09-13 13:32:27 +0300

    Created products controller

---
+++ backend/ShopHub/Api/ShopHub.Api/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿namespace ShopHub.Api.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using ShopHub.Services.Data.Contracts;
+    using ShopHub.Services.Models.Product;
+
+    //[Authorize]
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            this.productsService = productsService;   
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] ProductRequest request) 
+        {
+            IEnumerable<ProductResponse> products = await productsService.GetAll(request);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            ProductResponse product = await productsService.GetById(id);
+
+            if (product != null)
+            {
+                return Ok(product);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
