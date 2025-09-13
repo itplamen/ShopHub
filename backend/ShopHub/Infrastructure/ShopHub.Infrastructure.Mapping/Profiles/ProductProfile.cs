5342818271c5bb2cd153a6084f7d94ab7a546554
itplamen <itplamen@gmail.com>
2025-09-13 11:54:21 +0300

    Created mapping profiles

---
+++ backend/ShopHub/Infrastructure/ShopHub.Infrastructure.Mapping/Profiles/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopHub.Infrastructure.Mapping.Profiles
+{
+    using AutoMapper;
+
+    using ShopHub.Data.Models;
+    using ShopHub.Services.Models.Product;
+
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductResponse>();
+        }
+    }
+}
---
+++ backend/ShopHub/Infrastructure/ShopHub.Infrastructure.Mapping/Profiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopHub.Infrastructure.Mapping.Profiles
+{
+    using AutoMapper;
+    
+    using ShopHub.Data.Models;
+    using ShopHub.Services.Models.Auth;
+
+
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<RegisterRequest, User>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => DateTime.UtcNow));
+        }
+    }
+}
