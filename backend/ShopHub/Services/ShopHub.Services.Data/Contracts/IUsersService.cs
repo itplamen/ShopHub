f7a473089d7b6b2de0ad01d901db2b39e5631dc8
itplamen <itplamen@gmail.com>
2025-09-13 13:20:46 +0300

    Created users service

---
+++ backend/ShopHub/Services/ShopHub.Services.Data/Contracts/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopHub.Services.Data.Contracts
+{
+    using ShopHub.Services.Models.User;
+
+    public interface IUsersService
+    {
+        Task<UserResponse> GetById(int id);
+    }
+}
