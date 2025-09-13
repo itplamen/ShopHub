847abe6af42a872f54c77ff9afe6f9e00fd87063
itplamen <itplamen@gmail.com>
2025-09-14 02:48:34 +0300

    Extended the API

--- frontend/shop-hub-app/app/api/shopHubApi.ts
+++ frontend/shop-hub-app/app/api/shopHubApi.ts
@@ -1,16 +1,21 @@
-const post = async <TRequest, TResponse>(
+import type { ApiResponse } from "~/models/response";
+
+const getData = async <TResponse>(
   url: string,
-  body: TRequest
+  accessToken: string
 ): Promise<ApiResponse<TResponse>> => {
   try {
     const response = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
 
-    const errorData = await response.json();
-    return errorData;
+    const responseData = await response.json();
+
+    return responseData;
   } catch (error: unknown) {
     let message: string;
 
@@ -32,4 +37,39 @@ const post = async <TRequest, TResponse>(
   }
 };
 
-export { post };
+const postData = async <TRequest, TResponse>(
+  url: string,
+  body: TRequest
+): Promise<ApiResponse<TResponse>> => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const responseData = await response.json();
+
+    return responseData;
+  } catch (error: unknown) {
+    let message: string;
+
+    if (error instanceof Error) {
+      message = error.message;
+    } else if (error && typeof error === "object" && "message" in error) {
+      message = String(error.message);
+    } else if (typeof error === "string") {
+      message = error;
+    } else {
+      message = "Unknown error";
+    }
+
+    return {
+      isSuccess: false,
+      errors: [message],
+      data: null as TResponse,
+    };
+  }
+};
+
+export { postData, getData };
