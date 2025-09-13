ab2acde598df57c278a6cc27795ac8544b7cd5f7
itplamen <itplamen@gmail.com>
2025-09-13 13:19:34 +0300

    Created refresh token service

---
+++ backend/ShopHub/Services/ShopHub.Services.Data/Contracts/IRefreshTokensSerivce.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopHub.Services.Data.Contracts
+{
+    using ShopHub.Data.Models;
+
+    public interface IRefreshTokensSerivce
+    {
+        Task<string> Generate(int userId, string ipAddres, string deviceId, string deviceName);
+
+        Task<RefreshToken> Get(string token);
+
+        void Revoke(RefreshToken token);
+
+        Task SaveChanges();
+    }
+}
---
+++ backend/ShopHub/Services/ShopHub.Services.Data/RefreshTokensSerivce.cs
@@ -0,0 +1,51 @@
+﻿namespace ShopHub.Services.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+   
+    using ShopHub.Data.Contracts;
+    using ShopHub.Data.Models;
+    using ShopHub.Services.Data.Contracts;
+
+    public class RefreshTokensSerivce : IRefreshTokensSerivce
+    {
+        private readonly IConfiguration configuration;
+        private readonly IRepository<RefreshToken> repository;
+
+        public RefreshTokensSerivce(IConfiguration configuration, IRepository<RefreshToken> repository)
+        {
+            this.configuration = configuration;
+            this.repository = repository;
+        }
+
+        public async Task<string> Generate(int userId, string ipAddres, string deviceId, string deviceName)
+        {
+            var refreshToken = new RefreshToken()
+            {
+                Token = Guid.NewGuid().ToString(),
+                UserId = userId,
+                IPAddress = ipAddres,
+                DeviceId = deviceId,
+                DeviceName = deviceName,
+                ExpiryDate = DateTime.UtcNow.AddMinutes(int.Parse(configuration["Jwt:RefreshTokenValidityMins"]))
+            };
+
+            await repository.AddAsync(refreshToken);
+
+            return refreshToken.Token;
+        }
+
+        public async Task<RefreshToken> Get(string token)
+        {
+            RefreshToken refreshToken = await repository.Filter()
+                .Include(x => x.User)
+                .FirstOrDefaultAsync(x => x.Token == token && x.ExpiryDate > DateTime.UtcNow && x.DeletedOn == null);
+
+            return refreshToken;
+        }
+
+        public void Revoke(RefreshToken token) => repository.Delete(token);
+
+        public async Task SaveChanges() => await repository.SaveChangesAsync();
+    }
+}
