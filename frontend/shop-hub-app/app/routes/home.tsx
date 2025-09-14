e832cf2a4df410dc041e0a73ed640c6fc3d65c68
itplamen <itplamen@gmail.com>
2025-09-14 23:58:10 +0300

    Fixed url

--- frontend/shop-hub-app/app/routes/home.tsx
+++ frontend/shop-hub-app/app/routes/home.tsx
@@ -9,7 +9,7 @@ export const meta = ({}: Route.MetaArgs) => {
 };
 
 const ITEMS_PER_PAGE = 9;
-const PRODUCTS_URL = import.meta.env.VITE_PRODUCTS_API_URL;
+const API_AUTH_URL = import.meta.env.VITE_AUTH_API_URL;
 
 const Home = ({ loaderData }: Route.ComponentProps) => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -22,7 +22,7 @@ const Home = ({ loaderData }: Route.ComponentProps) => {
     try {
       setLoading(true);
       const res = await fetch(
-        `${PRODUCTS_URL}?page=${page}&pageSize=${ITEMS_PER_PAGE}`
+        `${API_AUTH_URL}/products?page=${page}&pageSize=${ITEMS_PER_PAGE}`
       );
       if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
       const data: Product[] = await res.json();
