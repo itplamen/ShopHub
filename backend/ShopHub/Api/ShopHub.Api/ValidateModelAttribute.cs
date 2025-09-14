29bfe04b971add25c5d18f15d88325824a0a3a34
itplamen <itplamen@gmail.com>
2025-09-14 03:07:56 +0300

    Added validation attribute

---
+++ backend/ShopHub/Api/ShopHub.Api/ValidateModelAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace ShopHub.Api
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    
+    using ShopHub.Services.Models.Base;
+
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                    .Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                context.Result = new BadRequestObjectResult(new BaseResponse { Errors = errors });
+            }
+        }
+    }
+}
