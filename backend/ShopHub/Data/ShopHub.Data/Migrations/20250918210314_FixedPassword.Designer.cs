4f4ad8e01d96cca7391b9cdfc3b1170d7804b4ef
itplamen <itplamen@gmail.com>
2025-09-19 00:03:48 +0300

    Added new migration

--- backend/ShopHub/Data/ShopHub.Data/Migrations/20250918210314_FixedPassword.Designer.cs
+++ backend/ShopHub/Data/ShopHub.Data/Migrations/20250918210314_FixedPassword.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopHub.Data;
 
@@ -11,9 +12,11 @@ using ShopHub.Data;
 namespace ShopHub.Data.Migrations
 {
     [DbContext(typeof(ShopHubDbContext))]
-    partial class ShopHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250918210314_FixedPassword")]
+    partial class FixedPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
